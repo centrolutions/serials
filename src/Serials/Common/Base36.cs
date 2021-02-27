baf24e68496ad98b33a3db3d6fbe913b08bc618a
Jason Williams <jason@centrolutions.com>
2021-02-27 14:11:16 -0600

    Initial check-in. Start of base36 decode / encode utility class.

---
+++ src/Serials/Common/Base36.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serials.Common
+{
+    public class Base36
+    {
+        private const string Alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static ulong Decode(string input)
+        {
+            ulong result = 0;
+            for (int i = input.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                var c = input[i];
+                var alphabetIndex = (ulong)Alphabet.IndexOf(c);
+                var multiplier = (ulong)Math.Pow(36, j);
+                result += alphabetIndex * multiplier;
+            }
+            return result;
+        }
+    }
+}
---
+++ tests/SerialsTests/Common/Base36Tests.cs
@@ -0,0 +1,34 @@
+﻿using Serials.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SerialsTests.Common
+{
+    public class Base36Tests
+    {
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("A", 10)]
+        [InlineData("Z", 35)]
+        public void Decode_ReturnsCorrectULong_WhenPassedASingleCharacter(string input, ulong expected)
+        {
+            ulong result = Base36.Decode(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("00", 0)]
+        [InlineData("10", 36)]
+        [InlineData("B01011", 665174629)]
+        public void Decode_ReturnsCorrectULong_WhenPassedMultipleCharacterString(string input, ulong expected)
+        {
+            ulong result = Base36.Decode(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
