4ea41cf49ccab68b04f425505b4e4d702b2515ff
Jason Williams <jason@centrolutions.com>
2021-02-27 15:04:26 -0600

    Added encode function to Base36 class Added parameter check on Decode function

--- src/Serials/Common/Base36.cs
+++ src/Serials/Common/Base36.cs
@@ -10,15 +10,36 @@ namespace Serials.Common
 
         public static ulong Decode(string input)
         {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentException("Cannot be null or empty", nameof(input));
+
+            var upperInput = input.ToUpper();
             ulong result = 0;
-            for (int i = input.Length - 1, j = 0; i >= 0; i--, j++)
+            for (int i = upperInput.Length - 1, j = 0; i >= 0; i--, j++)
             {
-                var c = input[i];
+                var c = upperInput[i];
                 var alphabetIndex = (ulong)Alphabet.IndexOf(c);
                 var multiplier = (ulong)Math.Pow(36, j);
                 result += alphabetIndex * multiplier;
             }
             return result;
+        }
+
+        public static object Encode(ulong input)
+        {
+            if (input == 0)
+                return "0";
+
+            var currentVal = input;
+            var result = new Stack<char>();
+            while (currentVal != 0)
+            {
+                var index = (int)(currentVal % 36);
+                result.Push(Alphabet[index]);
+                currentVal /= 36;
+            }
+
+            return new string(result.ToArray());
         }
     }
 }
--- tests/SerialsTests/Common/Base36Tests.cs
+++ tests/SerialsTests/Common/Base36Tests.cs
@@ -30,5 +30,45 @@ namespace SerialsTests.Common
 
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Decode_ReturnsCorrectULong_RegardlessOfCapitalization()
+        {
+            ulong capitalResult = Base36.Decode("ABC");
+            ulong lowerResult = Base36.Decode("abc");
+
+            Assert.Equal(capitalResult, lowerResult);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Decode_ThrowsArgumentException_WhenPassedEmptyOrNull(string input)
+        {
+            Assert.Throws<ArgumentException>(() => Base36.Decode(input));
+        }
+        
+
+        [Theory]
+        [InlineData(35, "Z")]
+        [InlineData(10, "A")]
+        [InlineData(1, "1")]
+        [InlineData(0, "0")]
+        public void Encode_ReturnsProperString_WhenPassedValueBelow36(ulong input, string expected)
+        {
+            var result = Base36.Encode(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(36, "10")]
+        [InlineData(665174629, "B01011")]
+        public void Encode_ReturnsEncodedString_WhenPassedLargerNumbers(ulong input, string expected)
+        {
+            var result = Base36.Encode(input);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
